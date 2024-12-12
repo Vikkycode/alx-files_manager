50e95238801f966c6f1b79b021034da70f8f3cb3
Victor <vicky.robilson5528@gmail.com>
2024-12-12 04:21:38 +0100

    redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,55 @@
 
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.log(err));
+  }
+
+  isAlive() {
+    return this.client.isReady;
+  }
+
+  async get(key) {
+    return new Promise((resolve) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          console.error(err);
+          resolve(null);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setEx(key, duration, value, (err, reply) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        resolve(reply);
+      });
+    });
+  }
+
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
